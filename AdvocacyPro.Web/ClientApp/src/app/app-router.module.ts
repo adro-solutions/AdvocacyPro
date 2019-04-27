5f415bb8587ffd56e7e99d444e36cd7bcf8ee3a0
Josh Knack <jknack@gmail.com>
2019-04-27 18:35:27 -0400

    add auth routes to route module

--- AdvocacyPro.Web/ClientApp/src/app/app-router.module.ts
+++ AdvocacyPro.Web/ClientApp/src/app/app-router.module.ts
@@ -1,9 +1,16 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { AppResolver } from './resolvers/app.resolver';
+import { LoginComponent } from './components/login.component';
+import { ChangePasswordComponent } from './components/changepassword.component';
+import { ResetPasswordComponent } from './components/resetpassword.component';
 
 const routes = [
-  { path: '', resolve: {app: AppResolver}, children: [] }
+  { path: '', resolve: {app: AppResolver}, children: [
+    { path: 'resetpassword', component: ResetPasswordComponent },
+    { path: 'changepassword', component: ChangePasswordComponent },
+    { path: 'login', component: LoginComponent }
+  ] }
 ];
 
 @NgModule({
